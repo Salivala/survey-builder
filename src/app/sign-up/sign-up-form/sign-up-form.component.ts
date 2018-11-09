e56ebe260494d5b4bd6facb2336f882c22a52c21
hsszyman <hsszyman@gmail.com>
2018-11-09 14:26:21 -0500

    Added username checking functionality in signup

--- src/app/sign-up/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up/sign-up-form/sign-up-form.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-form.component.scss']
 })
 export class SignUpFormComponent implements OnInit {
+  names: Array = ['hayden', 'bill'];
+  nameExists = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checkExists($events) {
+    const name = $events.target.value;
+    if (this.names.indexOf(name) > -1) {
+      this.nameExists = true;
+    } else {
+      this.nameExists = false;
+    }
   }
 
 }
